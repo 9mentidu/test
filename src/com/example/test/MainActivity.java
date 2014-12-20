5f175e9da0bd2b9d4c21054dfcdc233fb0ce72bc
luxiu.ch <luxiu.ch@ali-084338n.hz.ali.com>
2014-12-20 14:28:09 +0800

    test11

--- src/com/example/test/MainActivity.java
+++ src/com/example/test/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d("test", "test2");
+        Log.d("test", "test3");
     }
 
 
